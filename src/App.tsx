2e33f45ed42385c4d793c8ab32c606d32c0e1a21
LeeSeungHun <dos07008@naver.com>
2022-07-23 11:47:18 +0900

    지도 화면에 띄우기, 클릭 이벤트 추가, 좌표 > 주소 변환,마커 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [mapPoint, setMapPoint] = useState({ x: null, y: null });
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const mapDiv = document.getElementById("map");
+    const map = new window.naver.maps.Map(mapDiv);
+
+    window.naver.maps.Event.addDOMListener(mapDiv, "click", () => {
+      const coordinate = { x: map.data.map.center.x, y: map.data.map.center.y };
+      setMapPoint({ x: coordinate.x, y: coordinate.y });
+      new window.naver.maps.Marker({
+        position: map.getCenter(),
+        map: map,
+      });
+
+      window.naver.maps.Service.reverseGeocode(
+        {
+          coords: new window.naver.maps.LatLng(coordinate.y, coordinate.x),
+          orders: [
+            window.naver.maps.Service.OrderType.ADDR,
+            window.naver.maps.Service.OrderType.ROAD_ADDR,
+          ].join(","),
+        },
+        (
+          status: number,
+          response: {
+            v2: {
+              address: {
+                jibunAddress: string;
+              };
+            };
+          }
+        ) => {
+          if (status !== window.naver.maps.Service.Status.OK) {
+            return alert("Something wrong!");
+          }
+          const result = response.v2;
+          setLocation(result.address.jibunAddress);
+        }
+      );
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="map" style={{ width: "400px", height: "400px" }} />
+      <div>{mapPoint.x}</div>
+      <div>{mapPoint.y}</div>
+      <div>주소는 : {location}</div>
     </div>
   );
 }
