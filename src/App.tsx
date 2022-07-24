54e651035d606bbf80f8007c158035e352c94aca
LeeSeungHun <dos07008@naver.com>
2022-07-24 21:06:37 +0900

    마커 변경, 주소 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,20 @@ import { useEffect, useState } from "react";
 function App() {
   const [mapPoint, setMapPoint] = useState({ x: null, y: null });
   const [location, setLocation] = useState("");
-
+  console.log(location);
   useEffect(() => {
     const mapDiv = document.getElementById("map");
     const map = new window.naver.maps.Map(mapDiv);
 
     window.naver.maps.Event.addDOMListener(mapDiv, "click", () => {
       const coordinate = { x: map.data.map.center.x, y: map.data.map.center.y };
+
       setMapPoint({ x: coordinate.x, y: coordinate.y });
-      new window.naver.maps.Marker({
-        position: map.getCenter(),
-        map: map,
-      });
 
       window.naver.maps.Service.reverseGeocode(
         {
           coords: new window.naver.maps.LatLng(coordinate.y, coordinate.x),
+          // coords: map.getCenter(),
           orders: [
             window.naver.maps.Service.OrderType.ADDR,
             window.naver.maps.Service.OrderType.ROAD_ADDR,
@@ -39,6 +37,7 @@ function App() {
           }
           const result = response.v2;
           setLocation(result.address.jibunAddress);
+          console.log("결과값은", result.address.jibunAddress);
         }
       );
     });
@@ -46,7 +45,27 @@ function App() {
 
   return (
     <div>
-      <div id="map" style={{ width: "400px", height: "400px" }} />
+      <div
+        id="map"
+        style={{
+          width: "400px",
+          height: "400px",
+          zIndex: "2",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          alt="마커이미지"
+          src="https://cdn-icons.flaticon.com/png/512/5582/premium/5582962.png?token=exp=1658622788~hmac=587c3759f7797985f0494f8daabff31e"
+          style={{
+            position: "absolute",
+            zIndex: "1",
+            width: "30px",
+          }}
+        />
+      </div>
       <div>{mapPoint.x}</div>
       <div>{mapPoint.y}</div>
       <div>주소는 : {location}</div>
--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,4 @@ declare global {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
